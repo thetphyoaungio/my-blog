219bd475309e75e082acdaade3e5acb3863638ce
thetphyoaungio <kyawaung0003@gmail.com>
2024-12-07 11:47:06 +0630

    add app-ads.txt page for ShweMingalar app

---
+++ app/app-ads.txt/page.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+
+const ShweMingalarDhammaAppAdsTxt = () => {
+    return(
+        <>
+        google.com, pub-5889748970088125, DIRECT, f08c47fec0942fa0
+        </>
+    )
+}
+
+export default ShweMingalarDhammaAppAdsTxt;
